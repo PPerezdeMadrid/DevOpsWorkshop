8fea090f0221fe6df56847bee176d00fa81b5394
Paloma Pérez de Madrid <paloma.perezlaguna@beca.ceu.es>
2024-11-05 07:46:15 +0100

    Modificación en feature/practice1

--- devops-workshop-1/devops-workshop-1/index.js
+++ devops-workshop-1/devops-workshop-1/index.js
@@ -2,7 +2,7 @@ const app = require('./server');
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Servidor de Paloma http://localhost:${port}/`);
 });
 
 module.exports = server; // Exporta el servidor
